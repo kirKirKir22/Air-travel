6742fb032586cf01d807a1d0626c065871a1363a
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2024-01-27 18:30:11 +0300

    DepartureBeforeCurrentTimeFilter

---
+++ src/com/gridnine/testing/DepartureBeforeCurrentTimeFilter.java
@@ -0,0 +1,11 @@
+package com.gridnine.testing;
+
+import java.util.List;
+
+public class DepartureBeforeCurrentTimeFilter implements FlightFilter{
+
+    @Override
+    public List<Flight> filterFlights(List<Flight> flights) {
+        return null;
+    }
+}
