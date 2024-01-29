196c113ab8f72e79821fd274a2b6c0a88ebc42c5
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2024-01-29 17:52:52 +0300

    ArrivalBeforeDepartureFilter

---
+++ src/com/gridnine/testing/ArrivalBeforeDepartureFilter.java
@@ -0,0 +1,17 @@
+package com.gridnine.testing;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class ArrivalBeforeDepartureFilter implements FlightFilter {
+
+    @Override
+    public List<Flight> filterFlights(List<Flight> flights) {
+
+        return flights.stream()
+                .filter(flight -> flight.getSegments().stream()
+                        .allMatch(segment -> segment.getArrivalDate().isAfter(segment.getDepartureDate())))
+                .collect(Collectors.toList());
+    }
+
+}
