450048ec8c665df0be020bcff4c5ff8ac6013b15
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2024-02-01 00:45:07 +0300

    DepartureBeforeCurrentTimeFilter

--- src/com/gridnine/testing/DepartureBeforeCurrentTimeFilter.java
+++ src/com/gridnine/testing/DepartureBeforeCurrentTimeFilter.java
@@ -2,17 +2,20 @@ package com.gridnine.testing;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
 
 
 public class DepartureBeforeCurrentTimeFilter implements FlightFilter {
 
     @Override
     public List<Flight> filterFlights(List<Flight> flights) {
-            return flights.stream()
-                    .filter(flight -> flight.getSegments().stream()
-                            .allMatch(segment -> segment.getDepartureDate().isAfter(LocalDateTime.now())))
-                    .collect(Collectors.toList());
+        Predicate<Flight> departureBeforeNowPredicate = flight -> flight.getSegments().stream()
+                .allMatch(segment -> segment.getDepartureDate().isAfter(LocalDateTime.now()));
+
+        return flights.stream()
+                .filter(departureBeforeNowPredicate)
+                .collect(Collectors.toList());
     }
+
 }
--- src/com/gridnine/testing/FlightFilter.java
+++ src/com/gridnine/testing/FlightFilter.java
@@ -1,10 +1,16 @@
 package com.gridnine.testing;
 
-import com.gridnine.testing.Flight;
-
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public interface FlightFilter {
 
     List<Flight> filterFlights(List<Flight> flights);
+
+    default FlightFilter combine(Predicate<Flight> predicate) {
+        return flights -> flights.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+
 }
